e6ed2a35233063f08594d70b8817ed57c0278e27
SaNitYgRiM <85287339+SaNitYgRiM@users.noreply.github.com>
2021-12-12 23:31:25 +0530

    Update inheritanceSample.java
    //used super constructor

--- inheritanceSample.java
+++ inheritanceSample.java
@@ -1,5 +1,7 @@
-package Sample;
-import java.util.*;
+
+
+package SamplePgms;
+
 class Employee{
 	String name,Address;
 	int Age,PhoneNo,Salary;
@@ -8,61 +10,67 @@ class Employee{
 		System.out.println("Salary: "+Salary);
 		
 	}
+	Employee(String n,String A,int a,int p,int s)
+	{
+		name=n;
+		Address=A;
+		Age=a;
+		PhoneNo=p;
+		Salary=s;
+	}
 }
 class Officer extends Employee{
 	String specialisation;
-	void input()
-	{Scanner obj=new Scanner(System.in);
-		System.out.println("Enter name of the Officer:");
-		name=obj.next();
-		System.out.println("Enter Age of the Officer:");
-		Age=obj.nextInt();
-		System.out.println("Enter Phone no. of the Officer:");
-		PhoneNo=obj.nextInt();
-		System.out.println("Enter Address of the Officer:");
-		Address=obj.next();
-		System.out.println("Enter Salary of the Officer:");
-		Salary=obj.nextInt();
-		System.out.println("Enter Specialisation of the Officer:");
-		specialisation=obj.next();
-	}
+	
+	    Officer(String n,String A,int a,int p,int s,String S)
+	    {
+	    	super(n,A,a,p,s);
+		specialisation=S;
+	    }
+	    void display()
+	    {
+	    System.out.println("__________OFFICER:_________\nNAME:"+name+"\nAge:"+Age+"\nPhone no:"+PhoneNo+"\nAddress:"+Address+"");
+	    System.out.println("Specialisation:"+specialisation);
+
+		PrintSalary();
+	    }
 }
-class Manager extends Employee{
+class Manager extends Employee
+{
+	
 	String Department;
-	void Input()
+
+
+	Manager(String n,String A,int a,int p,int s,String d)
 	{
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter name of the Manager:");
-		name=obj.next();
-		System.out.println("Enter Age of the Manager:");
-		Age=obj.nextInt();
-		System.out.println("Enter Phone no. of the Manager:");
-		PhoneNo=obj.nextInt();
-		System.out.println("Enter Address of the Manager:");
-		Address=obj.next();
-		System.out.println("Enter Salary of the Manager:");
-		Salary=obj.nextInt();
-		System.out.println("Enter Department of the Manager:");
-		Department=obj.next();
+		super(n,A,a,p,s);
+		Department=d;
+		
 	}
+	void display()
+	{
+	System.out.println("\n__________MANAGER:_________\nNAME:"+name+"\nAge:"+Age+"\nPhone no:"+PhoneNo+"\nAddress:"+Address+"");
+	PrintSalary();
+	System.out.println("Department:"+Department);
+	}
+		
+	
 }
-public class inheritanceSample {
+
+public class Inheritancepgm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner obj=new Scanner(System.in);
-Officer of=new Officer();
-Manager M=new Manager();
-of.input();
-System.out.println("______________________");
-M.Input();
-System.out.println("__________OFFICER:_________\nNAME:"+of.name+"\nAge:"+of.Age+"\nPhone no:"+of.PhoneNo+"\nAddress:"+of.Address+"");
-of.PrintSalary();
-System.out.println("Specialisation:"+of.specialisation);
-System.out.println("\n__________MANAGER:_________\nNAME:"+M.name+"\nAge:"+M.Age+"\nPhone no:"+M.PhoneNo+"\nAddress:"+M.Address+"");
-M.PrintSalary();
-System.out.println("Department:"+M.Department);
+		
+		Officer of=new Officer("Mark","xywz",34,67787,34000,"hjgj");
+		Manager M=new Manager("Luke","jhgjz",30,7687,40000,"HR");
+		System.out.println("______________________");
+		of.display();
+		M.display();
+		
+
 
 	}
 
 }
+
