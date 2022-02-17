78c0e1a386d19d56250ea181321d6599166cc513
SaNitYgRiM <85287339+SaNitYgRiM@users.noreply.github.com>
2022-02-18 02:02:40 +0530

    Update Calculator.java
    this one works..but need to improvise

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,23 @@
 package SamplePgms;
 import javax.swing.*;
-
-
 import java.awt.event.*;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.awt.*;
 public class Calculator implements ActionListener {
 	
-	JTextField t1;	
-	JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,r,div,mul,add,sub,mod,c;
-	Calculator()
-	{
-		JFrame f=new JFrame("Calculator");
+		JFrame f;
+		JTextField t;	
+		JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,r,div,mul,add,sub,mod,c;
+		 
+		static double gt1=0,gt2=0,res=0;
+		static int opt=0;
+		 
+		Calculator()
+		{
+		f=new JFrame("Calculator");
 		JPanel p=new JPanel();
 		p.setBackground(Color.CYAN);
-		t1=new JTextField(15);
+		t=new JTextField(17);
 		b1=new JButton("1");
 		b2=new JButton("2");
 		b3=new JButton("3");
@@ -32,7 +35,8 @@ public class Calculator implements ActionListener {
 		mod=new JButton("%");
 		r=new JButton("=");
 		c=new JButton("C");
-		p.add(t1);
+
+		p.add(t);
 		p.add(b1);
 		p.add(b2);
 		p.add(b3);
@@ -50,12 +54,12 @@ public class Calculator implements ActionListener {
 		p.add(mod);
 		p.add(c);
 		p.add(r);
-		
-		
-		
+
 		f.setContentPane(p);
-		
-		
+		f.setSize(200,225);
+		f.setVisible(true);
+		f.setResizable(false);
+
 		b0.addActionListener(this);
 		b1.addActionListener(this);
 		b2.addActionListener(this);
@@ -73,144 +77,91 @@ public class Calculator implements ActionListener {
 		sub.addActionListener(this);
 		c.addActionListener(this);
 		r.addActionListener(this);
-	
-		f.setSize(200,225);
-		f.setVisible(true);
-		
-	}
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-	
-	int str1=0,str2,res=0,n=0,t = 0;String st1=null,s,st2 = null;	char ch;
-	//while(e.getSource()!=r)
-	{
-		if(e.getSource()==b1)
-	
-			t1.setText(t1.getText()+"1");
-
-		else if(e.getSource()==b2)
-
-			t1.setText(t1.getText()+"2");
-
-		else if(e.getSource()==b3)
-				t1.setText(t1.getText()+"3");
-
-		else if(e.getSource()==b4)
-       t1.setText(t1.getText()+"4");
-
-		else if(e.getSource()==b5)
-	    t1.setText(t1.getText()+"5");
-		else if(e.getSource()==b6)
-	    t1.setText(t1.getText()+"6");
-
-		else if(e.getSource()==b7)
-
-			t1.setText(t1.getText()+"7");
-
-		else if(e.getSource()==b8)
-			t1.setText(t1.getText()+"8");
-
-		else if(e.getSource()==b9)
-			t1.setText(t1.getText()+"9");
-
-		else if(e.getSource()==b0)
-			t1.setText(t1.getText()+"0");
-		
-		
-		else if(e.getSource()==add)
-		{
-		
-			String s2=t1.getText();
-			
-			str1=Integer.parseInt(s2);
-			
-			t1.setText(t1.getText()+"+");
-			//t1.setText(String.valueOf(str1));
-		/*	 while(t1.getText()!="\0")
-			 {
-				n++;
-			 if(t1.getText()=="+"){
-				t=n;
-				ch=t1.getText().charAt(n+1);
-				
-			 }
-			 
-			 } st2=t1.getText().substring(t+1,n);
-			 
-	
-			 str2=Integer.parseInt(st2);
-			res=str1+str2;
-			*/}
-			
-		
-		else if(e.getSource()==sub)
-		{
-			t1.setText(t1.getText()+"-");
-			res-=Integer.parseInt(t1.getText());
-			
 		}
-		else if(e.getSource()==mul)
+		 
+		public void actionPerformed(ActionEvent e)
+
 		{
-			t1.setText(t1.getText()+"*");
-			res*=Integer.parseInt(t1.getText());
+		try {
+			if(e.getSource()==b1)
+				t.setText(t.getText().concat("1"));
+				if(e.getSource()==b2)
+				t.setText(t.getText().concat("2"));
+				if(e.getSource()==b3)
+				t.setText(t.getText().concat("3"));
+				if(e.getSource()==b4)
+				t.setText(t.getText().concat("4"));
+				if(e.getSource()==b5)
+				t.setText(t.getText().concat("5"));
+				if(e.getSource()==b6)
+				t.setText(t.getText().concat("6"));
+				if(e.getSource()==b7)
+				t.setText(t.getText().concat("7"));
+				if(e.getSource()==b8)
+				t.setText(t.getText().concat("8"));
+				if(e.getSource()==b9)
+				t.setText(t.getText().concat("9"));
+				if(e.getSource()==b0)
+				t.setText(t.getText().concat("0"));
+
+				if(e.getSource()==add)
+				{
+				gt1=Double.parseDouble(t.getText());
+				opt=1;
+				t.setText("");
+				}
+				if(e.getSource()==sub)
+				{
+				gt1=Double.parseDouble(t.getText());
+				opt=2;
+				t.setText("");
+				}
+				if(e.getSource()==mul)
+				{
+				gt1=Double.parseDouble(t.getText());
+				opt=3;
+				t.setText("");
+				}
+				if(e.getSource()==div)
+				{
+				gt1=Double.parseDouble(t.getText());
+				opt=4;
+				t.setText("");
+				}
+				if(e.getSource()==mod)
+				{
+				gt1=Double.parseDouble(t.getText());
+				opt=5;
+				t.setText("");
+				}
+				if(e.getSource()==r)
+				{
+				gt2=Double.parseDouble(t.getText());
+				switch(opt)
+				{
+				case 1: res=gt1+gt2;
+				break;
+				case 2: res=gt1-gt2;
+				break;
+				case 3: res=gt1*gt2;
+				break;
+				case 4: res=gt1/gt2;
+				break;
+				case 5:res=gt1%gt2;
+				default: res=0;
+				}
+				t.setText(""+res);
+				}
+				if(e.getSource()==c)
+				t.setText("");
+
 			
-		}
-		else if(e.getSource()==mod)
+		}catch(ArithmeticException e1)
 		{
-			t1.setText(t1.getText()+"%");
-			res%=Integer.parseInt(t1.getText());
-			
+		    System.out.println(e1);
 		}
-		else if(e.getSource()==div)
-		{
-			t1.setText(t1.getText()+"/");
-			res/=Integer.parseInt(t1.getText());
-			
-		}
-		else if(e.getSource()==c)
-		{
-			t1.setText(" ");
-		}
-		
-		else if(e.getSource()==r)	
-			{
-			
-			String s2=t1.getText();
-			int l=s2.length();
-			
-			char[] ft=s2.toCharArray();
-			//int l=((CharSequence) t1).length();
-			 for(int i=0;i<l;i++)
-			 {
-				n++;
-			 if(ft[i]=='+'||ft[i]=='-'||ft[i]=='*'||ft[i]=='/'||ft[i]=='%') {
-		
-				t=n;
-				//.setText(String.valueOf(t));  t=2                 //just for checking
-			 }
-			 
-			 }//
-			 char[] ft1=new char[l-t];                                                //working till here
-			 for(int i=t,j=0;i<l;i++,j++)
-			 {                      
-				 
-				 ft1[j]= ft[i];
-				 
-			 }
-			 String s3=String.valueOf(ft1);
-			// t1.setText(s3);
-			 str2=Integer.parseInt(s3);
-			 
-			// st2=t1.getText().substring(t+1,l);
-			 res=str1+str2;
-			 t1.setText(String.valueOf(str1));
-			 //str2=Integer.parseInt(st2);
-			
-			 t1.setText(t1.getText()+"\n="+String.valueOf(res));;
-			}
-		else ;}
 	
-	}
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
